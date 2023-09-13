ae9e1d8fcc28d8fd5cbf5b0be38d5344f3dde7aa
Hans Bricks <hans@dialout.net>
2023-09-13 22:51:44 +0000

    add help cmd and QoL

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"flag"
 	"log"
-	"os"
-
+//	"os"
+        "fmt"
 	"github.com/simonlangowski/lightning1/client"
 	"github.com/simonlangowski/lightning1/config"
 	"github.com/simonlangowski/lightning1/errors"
@@ -11,10 +12,27 @@ import (
 )
 
 func main() {
-	serversFile := os.Args[1]
-	groupsFile := os.Args[2]
-	clientsFile := os.Args[3]
-	addr := os.Args[4]
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s <serversFile> <groupsFile> <clientsFile> <addr>\n", flag.CommandLine.Name())
+		fmt.Println("\nArguments:")
+		fmt.Println("  serversFile: Path to the JSON file containing server configurations.")
+		fmt.Println("  groupsFile: Path to the JSON file containing group configurations.")
+		fmt.Println("  clientsFile: Path to the JSON file containing client configurations.")
+		fmt.Println("  addr: Address for the client to bind to (e.g., localhost:8000).")
+	}
+
+	help := flag.Bool("help", false, "Display help")
+	flag.Parse()
+
+	if *help || len(flag.Args()) < 4 {
+		flag.Usage()
+		return
+	}
+
+	serversFile := flag.Arg(0)
+	groupsFile := flag.Arg(1)
+	clientsFile := flag.Arg(2)
+	addr := flag.Arg(3)
 	errors.Addr = addr
 	servers, err := config.UnmarshalServersFromFile(serversFile)
 	if err != nil {
