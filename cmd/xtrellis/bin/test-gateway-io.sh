924d9c8c60916874da88d06c357c6494e2e6d10d
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-10-03 22:24:23 -0700

    feat(bin): test-gateway-io.sh: gateway host/port as params

--- cmd/xtrellis/bin/test-gateway-io.sh
+++ cmd/xtrellis/bin/test-gateway-io.sh
@@ -10,6 +10,10 @@ set -e
 # bytes of data to send through the mix-net (default 10K)
 DATA_SIZE=${1:-10240}
 
+GATEWAY_HOST=${2:-localhost}
+GATEWAY_PORT_IN=${3:-9000}
+GATEWAY_PORT_OUT=${4:-9900}
+
 cd /tmp
 rm -f data.{in,out}
 
@@ -18,10 +22,10 @@ cat /dev/urandom | head -c ${DATA_SIZE} > data.in
 # dd if=/dev/urandom of=data.in bs=1024 count=10 &>/dev/null
 
 # file -> gateway -> [mix-net]
-cat data.in | nc -q 1 localhost 9000
+cat data.in | nc -q 1 ${GATEWAY_HOST} ${GATEWAY_PORT_IN}
 
 # [mix-net] -> gateway -> file
-wget -O data.out -q http://localhost:9900 || echo "not found"
+wget -O data.out -q http://${GATEWAY_HOST}:${GATEWAY_PORT_OUT} || echo "not found"
 
 # compare data input to output
 if diff -q data.in data.out &>/dev/null; then
