366ab3d045b5eb52011f8939b7e32cba731441d7
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-11-07 23:49:33 -0800

    refactor(docker): remote-network-simulator: create and use work dir

--- docker/remote-network-simulation/coordinator-run.sh
+++ docker/remote-network-simulation/coordinator-run.sh
@@ -2,11 +2,15 @@
 
 set -ex
 
+export _0KN_WORKDIR=~/.0KN
+mkdir -p ${_0KN_WORKDIR}
+cd ${_0KN_WORKDIR} # only required for coordinator
+
 # create hosts file, generate config from it, launch coordinator using it
 
 args='--numservers 3 --numgroups 3 --numusers 10 --groupsize 3 --numlayers 10'
-hostsfile='/src/go/trellis/cmd/experiments/ip.list'
+hostsfile="${_0KN_WORKDIR}/ip.list"
 
 echo -e "server-0\nserver-1\nserver-2" > ${hostsfile}
-cd /src/go/0kn/cmd/xtrellis && xtrellis coordinator config ${args} --hostsfile ${hostsfile}
-cd /src/go/trellis/cmd/coordinator && coordinator ${args} --runtype 2
+xtrellis coordinator config ${args} --hostsfile ${hostsfile}
+coordinator ${args} --runtype 2
