d7b9829bbcf0dfec1ab200303349987f24a6d4ff
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-11-18 16:27:02 -0800

    feat(docker): remote-network-simulator: test gateway using remote hosts

--- docker/remote-network-simulation/coordinator-run.sh
+++ docker/remote-network-simulation/coordinator-run.sh
@@ -5,11 +5,18 @@ set -ex
 export _0KN_WORKDIR=~/.0KN
 mkdir -p ${_0KN_WORKDIR}
 
-# create hosts file, generate config from it, launch coordinator using it
-
+# minimal mix-net args
 args='--numservers 3 --numgroups 3 --numusers 10 --groupsize 3 --numlayers 10'
-hostsfile="${_0KN_WORKDIR}/ip.list"
 
+# create hosts file for minimal mix-net
+hostsfile="${_0KN_WORKDIR}/hosts.list"
 echo -e "server-0:8000\nserver-1:8000\nserver-2:8000" > ${hostsfile}
+
+# generate mix-net round config from hosts file, share with each host
 xtrellis coordinator config ${args} --hostsfile ${hostsfile}
+
+# launch coordinated experiment using remote hosts
 xtrellis coordinator experiment ${args} --networktype 2
+
+# test gateway using remote hosts
+cd /src && ./scripts/test-gateway-ci.sh ${args} --networktype 2
