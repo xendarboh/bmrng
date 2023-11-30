6f70d60112c470ac7dfcc498db2cdc2f7ac548ff
Xendarboh <1435589+xendarboh@users.noreply.github.com>
2023-11-29 22:09:29 -0800

    feat(logger): add timestamp

--- go/0kn/pkg/logger/logger.go
+++ go/0kn/pkg/logger/logger.go
@@ -2,14 +2,20 @@ package logger
 
 import (
 	"encoding/json"
+	"time"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var (
 	Logger *zap.Logger
 	Sugar  *zap.SugaredLogger
 )
+
+func TimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006-01-02 15:04:05.000"))
+}
 
 func init() {
 	// TODO: get this from a config file
@@ -29,6 +35,9 @@ func init() {
 	if err := json.Unmarshal(rawJSON, &cfg); err != nil {
 		panic(err)
 	}
+
+	cfg.EncoderConfig.TimeKey = "ts"
+	cfg.EncoderConfig.EncodeTime = TimeEncoder
 
 	Logger := zap.Must(cfg.Build())
 	Sugar = Logger.Sugar()
